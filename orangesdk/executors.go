e99d824abe259973fc3264a23d6ceb545d1ba868
TralahM <briantralah@gmail.com>
2021-12-23 21:36:03 +0300

    log xml requests and responses
    Signed-off-by: TralahM <briantralah@gmail.com>

--- orangesdk/executors.go
+++ orangesdk/executors.go
@@ -63,7 +63,9 @@ func (f ExecutorFunc) Execute(r Request) Response {
 func (sdk *APIClient) Execute(r Request) Response {
 	var data bytes.Buffer
 	r.XML(&data)
+	sdk.logger.Println(data)
 	xmlBytes, err := sdk.Post(&data)
+	sdk.logger.Println(xmlBytes)
 	if err != nil {
 		sdk.logger.Fatalln(err)
 	}
@@ -74,7 +76,9 @@ func (sdk *APIClient) Execute(r Request) Response {
 func (sdk *APIClient) S2M(r Request) *s2mRESPONSE {
 	var data bytes.Buffer
 	r.XML(&data)
+	sdk.logger.Println(data)
 	xmlBytes, err := sdk.Post(&data)
+	sdk.logger.Println(xmlBytes)
 	if err != nil {
 		sdk.logger.Fatalln(err)
 	}
@@ -91,7 +95,9 @@ func (sdk *APIClient) S2M(r Request) *s2mRESPONSE {
 func (sdk *APIClient) M2S(r Request) *m2sRESPONSE {
 	var data bytes.Buffer
 	r.XML(&data)
+	sdk.logger.Println(data)
 	xmlBytes, err := sdk.Post(&data)
+	sdk.logger.Println(xmlBytes)
 	if err != nil {
 		sdk.logger.Fatalln(err)
 	}
@@ -108,7 +114,9 @@ func (sdk *APIClient) M2S(r Request) *m2sRESPONSE {
 func (sdk *APIClient) M2M(r Request) *m2mRESPONSE {
 	var data bytes.Buffer
 	r.XML(&data)
+	sdk.logger.Println(data)
 	xmlBytes, err := sdk.Post(&data)
+	sdk.logger.Println(xmlBytes)
 	if err != nil {
 		sdk.logger.Fatalln(err)
 	}
@@ -125,7 +133,9 @@ func (sdk *APIClient) M2M(r Request) *m2mRESPONSE {
 func (sdk *APIClient) Callback(r Request) *callbackurlRESPONSE {
 	var data bytes.Buffer
 	r.XML(&data)
+	sdk.logger.Println(data)
 	xmlBytes, err := sdk.Post(&data)
+	sdk.logger.Println(xmlBytes)
 	if err != nil {
 		sdk.logger.Fatalln(err)
 	}
@@ -142,7 +152,9 @@ func (sdk *APIClient) Callback(r Request) *callbackurlRESPONSE {
 func (sdk *APIClient) CheckBal(r Request) *checkbalanceRESPONSE {
 	var data bytes.Buffer
 	r.XML(&data)
+	sdk.logger.Println(data)
 	xmlBytes, err := sdk.Post(&data)
+	sdk.logger.Println(xmlBytes)
 	if err != nil {
 		sdk.logger.Fatalln(err)
 	}
@@ -159,7 +171,9 @@ func (sdk *APIClient) CheckBal(r Request) *checkbalanceRESPONSE {
 func (sdk *APIClient) CheckTrans(r Request) *checktransactionstatusRESPONSE {
 	var data bytes.Buffer
 	r.XML(&data)
+	sdk.logger.Println(data)
 	xmlBytes, err := sdk.Post(&data)
+	sdk.logger.Println(xmlBytes)
 	if err != nil {
 		sdk.logger.Fatalln(err)
 	}
@@ -176,7 +190,9 @@ func (sdk *APIClient) CheckTrans(r Request) *checktransactionstatusRESPONSE {
 func (sdk *APIClient) SendSMS(r Request) *sendsmsRESPONSE {
 	var data bytes.Buffer
 	r.XML(&data)
+	sdk.logger.Println(data)
 	xmlBytes, err := sdk.Post(&data)
+	sdk.logger.Println(xmlBytes)
 	if err != nil {
 		sdk.logger.Fatalln(err)
 	}
